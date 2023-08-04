841c9fcbc08a5c69c7199ca76d484b5eda5ec0a9
Edmond <mredmondwang@gmail.com>
2023-08-04 10:35:55 +0800

    Redesign wallet cards

--- FrontEnd/components/CardCard.js
+++ FrontEnd/components/CardCard.js
@@ -1,6 +1,8 @@
-import { View, Text, TouchableOpacity } from "react-native";
+import { View, Text, TouchableOpacity, Image } from "react-native";
 import React from "react";
 import { FontAwesome } from "@expo/vector-icons"
+
+import mastercard from "../assets/mastercard.png";
 
 const CardCard = ({
     name,
@@ -15,7 +17,7 @@ const CardCard = ({
   return (
     <View>
       <TouchableOpacity className="bg-white h-60 rounded-2xl mt-4">
-        <View className="flex-col flex-1 px-8 pt-6 pb-2">
+        <View className="flex-col flex-1 px-4 pt-6 pb-2">
 
           {/* Top Portion */}
           <View className="flex-row space-x-4 flex-1">
@@ -26,13 +28,16 @@ const CardCard = ({
             </Text>
           </View>
           
-          <Text className="text-3xl font-bold">{cardNumber}</Text>
-          <View className='flex-row'>
-            <Text className='font-semibold'>CVV: {cvv}</Text>
-            <Text className='pl-28 text-lg'>EXP: {expDate.substring(5, 7)}/{expDate.substring(2,4)}</Text>
+          <Text className="text-3xl font-bold pl-4 ">{cardNumber}</Text>
+          <View className='flex-row pt-2 pl-4'>
+            <Text className='font-semibold self-end flex-1'>CVV: {cvv}</Text>
+            <Text className='text-[10px] self-end'>VALID{'\n'}THRU </Text>
+            <Text className='text-xl self-end'>{expDate.substring(5, 7)}/{expDate.substring(2,4)}</Text>
+            <View className='flex-1'></View>
           </View>
-          <View className="pt-2 flex-row">
-            <Text className='text-lg'>{accountHolder.toUpperCase()}</Text>
+          <View className="pt-2 flex-row ">
+            <Text className='text-xl flex-1 self-end'>{accountHolder.toUpperCase()}</Text>
+            <Image source={mastercard} className='w-[80] h-[50]'/>
           </View>
         </View>
       </TouchableOpacity>
