1daa9f8be52d84ab914e5d0d932ef29172fcb35f
Edmond <mredmondwang@gmail.com>
2023-08-19 22:43:20 +0800

    Deploy/Install more packages

--- BackEnd/lambda.js
+++ BackEnd/lambda.js
@@ -1,5 +1,8 @@
 require('source-map-support/register')
 const serverlessExpress = require('@vendia/serverless-express')
-const app = require('./app')
+// const app = require('./app')
+
+
+import app from "server.mjs"
 
 exports.handler = serverlessExpress({ app })
--- BackEnd/server.mjs
+++ BackEnd/server.mjs
@@ -35,7 +35,9 @@ app.use(session({
 
 app.use("/api", routes);
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port: ${PORT}`);
-});
+export default app;
+
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port: ${PORT}`);
+// });
  
