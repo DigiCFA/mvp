f10ac4d1213cf7ac8735e3c0621190df26876a34
LiuHenry9800 <henryliu9800@gmail.com>
2023-08-20 15:49:19 -0700

    searchbar backend update-atlas

--- BackEnd/src/routes/profile.mjs
+++ BackEnd/src/routes/profile.mjs
@@ -53,8 +53,19 @@ router.get("/search_users", async (req, res) => {
   let query = req.body.query;
   try {
     // all transactions
-    let result = await User.fuzzy_search(query)
-      .project({firstName:1,lastName:1,fullName:1,phoneNumber:1,_id:1})
+    const result = await User.aggregate().
+    search({
+      text: {
+        query: query,
+      }
+    });
+   
+
+
+
+
+    //let result = await User.fuzzy_search(query)
+      //.project({firstName:1,lastName:1,fullName:1,phoneNumber:1,_id:1})
     res.status(200).send(result);
   } catch (error) {
     console.error(error);
