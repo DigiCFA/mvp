630c41199a3dde5f7b39fad544dc7ae829cc9d42
Edmond <mredmondwang@gmail.com>
2023-08-30 13:32:38 +0800

    Set up bar code scanner plugin for vision camera

--- FrontEnd/screens/Transfer/ScanScreen.js
+++ FrontEnd/screens/Transfer/ScanScreen.js
@@ -1,24 +1,56 @@
-import { StyleSheet, View, Text, SafeAreaView, TouchableOpacity } from "react-native";
+import { StyleSheet, View, Text, SafeAreaView, TouchableOpacity, Alert } from "react-native";
 import {Ionicons} from "@expo/vector-icons"
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useIsFocused, useNavigation } from "@react-navigation/native";
 import { Camera, useCameraDevices } from "react-native-vision-camera";
 import LoadingView from "../../components/LoadingView";
+import { useScanBarcodes } from "vision-camera-code-scanner";
+
+const checkCameraPermission = async() => {
+  let status = await Camera.getCameraPermissionStatus();
+  if (status !== 'authorized') {
+    await Camera.requestCameraPermission();
+    status = await Camera.getCameraPermissionStatus();
+    console.log("Camera Authorised")
+    setHasPermission(status === 'authorized');
+    if (status === 'denied') {
+      Alert.alert('No Camera Access', 'You need to give permission to use the QR Code scanner', [{
+        text: 'Cancel',
+        style: 'cancel'
+      }, {
+        text: 'OK'
+      }])
+    }
+  }
+}
 
 const ScanScreen = () => {
 
   const navigation = useNavigation();
-
   const isFocused = useIsFocused();
 
-  // const cameraPermission = await Camera.getCameraPermissionStatus();
-
+  const [hasPermission, setHasPermission] = useState(false);
   const devices = useCameraDevices();
   const device = devices.back;
 
-  if (device == null) return <LoadingView />
+  const [frameProcessor, barcodes] = useScanBarcodes([BarcodeFormat.QR_CODE], {
+    checkInverted: true,
+  })
+
+  // useEffect(() => {
+  //   (async () => {
+  //     const status = await Camera.requestCameraPermission();
+  //     console.log("Camera Authorised")
+  //     setHasPermission(status === 'authorized');
+  //   })();
+  // }, []);
+
+  useEffect(() => {
+    checkCameraPermission();
+  }, [])
 
   return (
+
     <SafeAreaView>
       {/* Top Bar */}
       <View className="flex-row justify-items-start items-center space-x-2 pt-1 mx-4">
@@ -29,16 +61,31 @@ const ScanScreen = () => {
           Scan Screen
         </Text>
 
-        <Camera 
-          style={StyleSheet.absoluteFill}
-          device={device}
-          isActive={isFocused}
-        />
+        {device != null && hasPermission && (
+          <>
+            <Camera 
+              style={StyleSheet.absoluteFill}
+              device={device}
+              isActive={isFocused}
+              frameProcessor={frameProcessor}
+              frameProcessorFps={5}
+            />
+            {
+              barcodes.map((barcode, idx) => {
+                <Text key={idx} className='text-lg text-white font-bold'>
+                  {barcode.displayValue}
+                </Text>
+              })
+            }
+          </>
+        )}
+
 
         <View className="flex-1"></View>
       </View>
     </SafeAreaView>
   );
 };
+
 
 export default ScanScreen;
