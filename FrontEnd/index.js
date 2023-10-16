f5cc770085e53b9dad77c8f8fb78d19d180c96bb
LiuHenry9800 <henryliu9800@gmail.com>
2023-10-15 17:16:19 -0700

    start code receive data

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -1,8 +1,15 @@
 // import 'react-native-reanimated'
 import { registerRootComponent } from "expo";
+import { AppRegistry } from 'react-native';
+import FCM from '@react-native-firebase/messaging';
 
 import App from "./src/App";
 
+
+
+FCM().setBackgroundMessageHandler(async (sentData)=>{
+    console.log(sentData);
+})
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
