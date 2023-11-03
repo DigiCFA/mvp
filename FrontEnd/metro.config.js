cd21bf6310b76faac93567c032f0755a2b1bcab0
Edmond <mredmondwang@gmail.com>
2023-11-03 15:10:05 -0700

    Reconfigure project ID to the shared Expo Organization

--- FrontEnd/metro.config.js
+++ FrontEnd/metro.config.js
@@ -1,4 +1,9 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require('expo/metro-config');
 
-module.exports = getDefaultConfig(__dirname);
+const defaultConfig = getDefaultConfig(__dirname);
+
+// Need to do this for resolvers
+// defaultConfig.resolver.assetExts.push('db');
+
+module.exports = defaultConfig;
