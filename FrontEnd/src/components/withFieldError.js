98d130ffc441e8b1682ba9e2db11a08abd8c01bd
Daniel Bai <daniel.bai@hotmail.com>
2023-12-27 17:39:40 +0800

    Attemp to fix internal distribution error

--- FrontEnd/src/components/withFieldError.js
+++ FrontEnd/src/components/withFieldError.js
@@ -1,5 +1,5 @@
 import { View, Text } from "react-native";
-import React, { useEffect, useLayoutEffect, useState } from "react";
+import React, { useState } from "react";
 import { FontAwesome, FontAwesome5 } from "@expo/vector-icons";
 
 const withFieldError = (WrappedComponent) => {
